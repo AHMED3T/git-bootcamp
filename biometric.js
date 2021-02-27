6f576b996a8c9b835260fbdf51411870b8c8278d
AHMED3T <ahmedrasool@dedevtech.com>
2021-02-27 15:02:33 +0500

    master changes commited

--- biometric.js
+++ biometric.js
@@ -1,9 +1,10 @@
-const a = 5;
-const b = 10;
-
-
-
-
-const a = require(`fs`);
-const b = a.readFile();
+console.log(`Biometric 2.0`); console.log(`Biometric 2.0`); console.log(`Biometric 2.0`); console.log(`Biometric 2.0`); console.log(`Biometric 2.0`); console.log(`Biometric 2.0`); console.log(`Biometric 2.0`); console.log(`Biometric 2.0`); console.log(`Biometric 2.0`); console.log(`Biometric 2.0`); console.log(`Biometric 2.0`); console.log(`Biometric 2.0`); console.log(`Biometric 2.0`); console.log(`Biometric 2.0`); console.log(`Biometric 2.0`); console.log(`Biometric 2.0`); console.log(`Biometric 2.0`); console.log(`Biometric 2.0`);
+console.log(`Biometric 2.0`);
+console.log(`Biometric 2.0`);
+console.log(`Biometric 2.0`);
+console.log(`Biometric 2.0`);
+console.log(`Biometric 2.0`);
+console.log(`Biometric 2.0`);
+console.log(`Biometric 2.0`);
+console.log(`Biometric 2.0`);
 console.log(`Biometric 2.0`);

6ae6e1257b2ad61fffadec54912e4d330e9942b4
AHMED3T <ahmedrasool@dedevtech.com>
2021-02-27 15:01:52 +0500

    changes added

--- biometric.js
+++ biometric.js
@@ -1,9 +1,19 @@
 const a = 5;
 const b = 10;
+const a = 5;
+const b = 10;
+const a = 5;
+const b = 10;
 
 
 
 
+const a = require(`fs`);
+const b = a.readFile();
+const a = require(`fs`);
+const b = a.readFile();
+const a = require(`fs`);
+const b = a.readFile();
 const a = require(`fs`);
 const b = a.readFile();
 console.log(`Biometric 2.0`);

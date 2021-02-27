1dd170dc31fc47d979b19d032359be803cff2415
AHMED3T <ahmedrasool@dedevtech.com>
2021-02-27 15:17:07 +0500

    hotbug fixed

--- biometric.js
+++ biometric.js
@@ -3,10 +3,6 @@ console.log(`Biometric 2.0`);
 console.log(`Biometric 2.0`);
 console.log(`Biometric 2.0`);
 console.log(`Biometric 2.0`);
-console.log(`Biometric 2.0`);
-console.log(`Biometric 2.0`);
-console.log(`Biometric 2.0`);
-console.log(`Biometric 2.0`);
 const a = 5;
 const b = 10;
 const a = 5;
@@ -14,7 +10,13 @@ const b = 10;
 const a = 5;
 const b = 10;
 
+const d = () => {
+  try {
 
+  } catch (error) {
+
+  }
+};
 
 
 const a = require(`fs`);
